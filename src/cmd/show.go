faca07dcb55d0669980dd7ee752f7357628ce5d4
Oskar Jung <jung.oskar@gmail.com>
2020-11-12 10:11:18 +0100

    Pull before show

--- src/cmd/show.go
+++ src/cmd/show.go
@@ -16,6 +16,11 @@ var showCmd = &cobra.Command{
 	Short: "Show all commands from tome.",
 	Run: func(cmd *cobra.Command, args []string) {
 		repo := tome.NewGitRepository(viper.GetString(tome.REPOSITORY_CONFIG_KEY))
+		err := tome.Pull()
+		if err != nil && err.Error() != "already up-to-date" {
+			tome.Check(err)
+		}
+
 		lines, err := repo.GetAll()
 		tome.Check(err)
 
