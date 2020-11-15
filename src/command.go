f8d738c79dc69dfaecff18f0c362d2626e773fd2
Jan Graßegger <gesundkrank@mailbox.org>
2020-11-15 21:54:24 +0100

    Parse Commands when loading from file
    - move Command code to seperate file

--- src/command.go
+++ src/command.go
@@ -2,7 +2,12 @@ package tome
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"regexp"
+	"strconv"
 	"strings"
+	"text/scanner"
 )
 
 // Command is a tome Command with its metadata
@@ -12,6 +17,8 @@ type Command struct {
 	Tags      []string
 	Command   string
 }
+
+var itemRegex = regexp.MustCompile("(?s)(?P<timestamp>[[:digit:]]+);(?P<author>[^;]+);(?P<tags>(?:[[:word:]]+:?)*);(?P<command>.+)")
 
 func NewCommand(timestamp int64, author string, tags []string, command string) Command {
 	return Command{
@@ -26,4 +33,54 @@ func NewCommand(timestamp int64, author string, tags []string, command string) C
 func (c Command) String() string {
 	t := strings.Join(c.Tags, ":")
 	return fmt.Sprintf(": %d;%s;%s;%s", c.Timestamp, c.Author, t, c.Command)
+}
+
+func ParseCommands(reader io.Reader) ([]Command, error) {
+	var s scanner.Scanner
+	s.Init(reader)
+	s.Whitespace ^= 1<<'\t' | 1<<'\n' | 1<<' '
+
+	lastRune := '\n'
+	s.IsIdentRune = func(ch rune, i int) bool {
+		oldLastRune := lastRune
+		lastRune = ch
+		return !(ch == ':' && oldLastRune == '\n' || ch == ' ' && i == 0 || ch == scanner.EOF)
+	}
+
+	var items []Command
+
+	for token := s.Scan(); token != scanner.EOF; token = s.Scan() {
+		if token == ':' || token == ' ' {
+			continue
+		}
+		text := s.TokenText()
+
+		match := itemRegex.FindStringSubmatch(text)
+
+		if match == nil {
+			log.Printf("failed to parse '%s' as Command", text)
+			continue
+		}
+
+		timestamp, err := strconv.ParseInt(match[itemRegex.SubexpIndex("timestamp")], 10, 64)
+		if err != nil {
+			log.Print(err.Error())
+			continue
+		}
+
+		author := match[itemRegex.SubexpIndex("author")]
+		tagsString := match[itemRegex.SubexpIndex("tags")]
+
+		var tags []string
+		if len(tagsString) > 0 {
+			tags = strings.Split(tagsString, ":")
+		}
+
+		command := strings.TrimRight(match[itemRegex.SubexpIndex("command")], "\n")
+		items = append(items, NewCommand(timestamp, author, tags , command))
+
+	}
+
+	return items, nil
+
 }
--- src/command_test.go
+++ src/command_test.go
@@ -1,6 +1,8 @@
 package tome
 
 import (
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -25,6 +27,49 @@ func TestCommand_String(t *testing.T) {
 			result := tt.input.String()
 			if result != tt.expected {
 				t.Errorf("'%s' does not equal '%s'", result, tt.expected)
+			}
+		})
+	}
+}
+
+func TestParseCommands(t *testing.T) {
+
+	singleLineCommandString := ": 1605292765;Oskar Jung;tag;fg"
+	singleLineCommand := NewCommand(1605292765, "Oskar Jung", []string{"tag"}, "fg")
+	multiLineCommandString := ": 1605290038;Oskar Jung;multiline;echo \"asd\\\nfgh\""
+	multiLineCommand := NewCommand(1605290038, "Oskar Jung", []string{"multiline"},
+		"echo \"asd\\\nfgh\"")
+	invalidCommandString := ": 1605292765;Oskar Jung;fg"
+
+	table := []struct {
+		testName string
+		input    string
+		expected []Command
+	}{
+		{"parse empty file without error", "", nil},
+		{"parse command", singleLineCommandString, []Command{singleLineCommand}},
+		{"ignore invalid line", invalidCommandString, nil},
+		{"parse multiline command", multiLineCommandString, []Command{multiLineCommand}},
+		{"parse multiple commands",
+			strings.Join([]string{multiLineCommandString, invalidCommandString, singleLineCommandString}, "\n"),
+			[]Command{multiLineCommand, singleLineCommand}},
+		{"parse only commands that start with newline",
+			singleLineCommandString + " " + singleLineCommandString,
+			[]Command{NewCommand(1605292765, "Oskar Jung", []string{"tag"},
+				"fg "+singleLineCommandString)},
+		},
+	}
+
+	for _, tt := range table {
+		t.Run("should "+tt.testName, func(t *testing.T) {
+			result, err := ParseCommands(strings.NewReader(tt.input))
+
+			if err != nil {
+				t.Errorf("Failed to parse '%s'. Error: %s", result, err.Error())
+			}
+
+			if !reflect.DeepEqual(result, tt.expected) {
+				t.Errorf("'%s' did not match expected result '%s'", result, tt.expected)
 			}
 		})
 	}
