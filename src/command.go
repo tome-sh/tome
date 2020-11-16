487fb92b3964cf37c507c887f694c07335941efb
Jan Graßegger <gesundkrank@mailbox.org>
2020-11-16 21:44:48 +0100

    Order Command fields by serialization order

--- src/command.go
+++ src/command.go
@@ -5,25 +5,24 @@ import (
 	"strings"
 )
 
-// Command is a tome command.
+// Command is a tome Command with its metadata
 type Command struct {
-	Author    string
-	Command   string
 	Timestamp int64
+	Author    string
 	Tags      []string
+	Command   string
 }
 
-// NewFileRepository creates a new FileRepository.
-func NewCommand(author string, command string, timestamp int64, tags []string) Command {
+func NewCommand(timestamp int64, author string, tags []string, command string) Command {
 	return Command{
-		Author:    author,
-		Command:   command,
 		Timestamp: timestamp,
+		Author:    author,
 		Tags:      tags,
+		Command:   command,
 	}
 }
 
-//String() is the string representation of a command.
+//String() is the string representation of a Command.
 func (c Command) String() string {
 	t := strings.Join(c.Tags, ":")
 	return fmt.Sprintf("%d;%s;%s;%s", c.Timestamp, c.Author, t, c.Command)
