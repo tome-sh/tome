8084b07cf49b4c5e96b5dc6a1c8e9122c2ca0535
Oskar Jung <jung.oskar@gmail.com>
2020-11-19 23:22:23 +0100

    Use built-in cut command

--- shell/key-bindings.zsh
+++ shell/key-bindings.zsh
@@ -2,7 +2,7 @@
   [[ -o interactive ]] || return 0
 
   tome-show-widget() {
-    selected=$(tome show | fzf --query=${LBUFFER} --with-nth=5..100 --delimiter=';' | awk '{split($0, a, ";"); print a[1]}')
+    selected=$(tome show | fzf --query=${LBUFFER} --with-nth=5..100 --delimiter=';' | cut -d ';' -f 1)
     local ret=$?
     BUFFER=$(tome get --id "$selected")
     zle reset-prompt
